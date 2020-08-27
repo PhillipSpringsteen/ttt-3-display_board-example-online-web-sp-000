ab07c2cc9807c33f2bed31f9d5e855e38841e075
Oscar Phillip Springsteen <pspringsteen160@gmail.com>
2020-08-27 07:56:43 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,8 +1,9 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
-r1="   |   |   "
-r2="-----------"
-puts r1
-puts r2
-puts r1
-puts r2
-puts r1
+def display_board
+  r1="   |   |   "
+  r2="-----------"
+  puts r1
+  puts r2
+  puts r1
+  puts r2
+  puts r1
